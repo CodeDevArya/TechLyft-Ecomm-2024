3b116d7c0c874d85f9d20b50741d9117f39f105e
CodeDevArya <aryah144@gmail.com>
2024-09-23 09:05:27 +0530

    final commit

--- ecommerce-backend/dist/app.js
+++ ecommerce-backend/dist/app.js
@@ -51,12 +51,12 @@ app.use("/api/v1/admin-panel/stats", StatsRoute);
 app.use("/uploads", express.static("uploads"));
 app.use(errorMiddleware);
 //
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//     });
+// }
 app.listen(port, () => {
     console.log(`Server is running on  http://localhost:${port}`);
 });
